1e4eb4ad2dad7ab57c5d038266564c7f5753ff30
Luís Teixeira <micboucinha@gmail.com>
2018-11-27 14:21:38 +0000

    Fix for UUIDs errors when saving

--- pybpodgui_api/models/session/session_io.py
+++ pybpodgui_api/models/session/session_io.py
@@ -40,8 +40,8 @@ class SessionIO(SessionBase):
         data.update({'started': str(self.started.strftime('%Y%m%d-%H%M%S')) if self.started else None})
         data.update({'ended': str(self.ended.strftime('%Y%m%d-%H%M%S')) if self.ended else None})
         data.update({'setup': self.setup.uuid4})
-        data.update({'task': self.task.uuid4 if self.task else None})
-        data.update({'board': self.setup.board.uuid4})
+        data.update({'task': str(self.task.uuid4 if self.task else None)})
+        data.update({'board': str(self.setup.board.uuid4 if self.setup.board else None)})
         data.update({'serial_port': self.setup.board.serial_port})
 
         data.update({'subjects': []})
